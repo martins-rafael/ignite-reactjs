1a735b8033d7a29a079b7e46cc7bf6d33986940a
Rafael Martins <rafaeldcmartins@gmail.com>
2021-03-09 17:25:06 -0300

    useEffect hook

--- src/components/RepositoryList.js
+++ src/components/RepositoryList.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import RepositoryItem from './RepositoryItem';
 
 import '../styles/repositories.scss';
@@ -8,17 +9,27 @@ const repository = {
   link: 'https://github.com/martins-rafael/moveIt',
 };
 
-const RepositoryList = () => (
-  <section className="repository-list">
-    <h1>Lista de repositórios</h1>
+const RepositoryList = () => {
+  const [repositories, setRepositories] = useState([]);
 
-    <ul>
-      <RepositoryItem repository={repository} />
-      <RepositoryItem repository={repository} />
-      <RepositoryItem repository={repository} />
-      <RepositoryItem repository={repository} />
-    </ul>
-  </section>
-);
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, []);
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+      </ul>
+    </section>
+  );
+}
 
 export default RepositoryList;
