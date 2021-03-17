4903a9b68184308136d349c91e655c48322b62ac
Rafael Martins <rafaeldcmartins@gmail.com>
2021-03-17 10:05:02 -0300

    Modal & Forms - config modal

--- 02-primeira-aplicacao-reactjs/src/components/Header/index.tsx
+++ 02-primeira-aplicacao-reactjs/src/components/Header/index.tsx
@@ -2,13 +2,19 @@ import logoImg from '../../assets/logo.svg';
 
 import { Container, Content } from './styles';
 
-export const Header = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="dt money" />
-      <button type="button">
-        Nova Transação
-      </button>
-    </Content>
-  </Container>
-);
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export const Header = ({ onOpenNewTransactionModal }: HeaderProps) => {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova Transação
+        </button>
+      </Content>
+    </Container>
+  );
+}
