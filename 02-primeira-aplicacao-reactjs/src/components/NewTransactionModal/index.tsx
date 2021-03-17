aa2a4b24480088d7abbf03fd28697ade56ab03c5
Rafael Martins <rafaeldcmartins@gmail.com>
2021-03-17 10:22:48 -0300

    Modal & Forms - NewTransactionModal

---
+++ 02-primeira-aplicacao-reactjs/src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,20 @@
+import Modal from 'react-modal';
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export const NewTransactionModal = ({
+  isOpen,
+  onRequestClose
+}: NewTransactionModalProps) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+    >
+      <h2>Cadastrar Transação</h2>
+    </Modal>
+  );
+};
