9e340502c63f4e330c166ca04fedd85d430e7a42
Rafael Martins <rafaeldcmartins@gmail.com>
2021-03-24 10:48:25 -0300

    Components and Pages - Subscribe button

---
+++ 03-fundamentos-next/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.scss';
+
+export const SubscribeButton = () => (
+  <button className={styles.subscribeButton} type="button">
+    Subscribe now
+  </button>
+);
--- 03-fundamentos-next/src/pages/index.tsx
+++ 03-fundamentos-next/src/pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from 'next/head';
+
+import { SubscribeButton } from '../components/SubscribeButton';
+
 import styles from './index.module.scss';
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <Head>
@@ -11,11 +14,13 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome!</span>
-          <h1>News about <br/> the <span>React</span> world.</h1>
+          <h1>News about <br /> the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -23,3 +28,5 @@ export default function Home() {
     </>
   );
 }
+
+export default Home;
