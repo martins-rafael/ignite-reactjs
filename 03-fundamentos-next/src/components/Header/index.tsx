711b77550645fa50c6af8fa1680460a794ea0cae
Rafael Martins <rafaeldcmartins@gmail.com>
2021-03-24 09:30:05 -0300

    Components and Pages - header

---
+++ 03-fundamentos-next/src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.scss';
+
+export const Header = () => (
+  <header className={styles.headerContainer}>
+    <div className={styles.headerContent}>
+      <img src="/images/logo.svg" alt="ig.news" />
+
+      <nav>
+        <a href="" className={styles.active}>Home</a>
+        <a href="">Posts</a>
+      </nav>
+    </div>
+  </header>
+);
--- 03-fundamentos-next/src/pages/_app.tsx
+++ 03-fundamentos-next/src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import { AppProps } from 'next/app';
+import { Header } from '../components/Header';
 
 import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Header />
+    <Component {...pageProps} />
+  </>
+);
 
 export default MyApp
