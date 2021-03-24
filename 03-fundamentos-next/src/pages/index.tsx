29baca2e7e132793d108dbaa5ea41bead9681f41
Rafael Martins <rafaeldcmartins@gmail.com>
2021-03-24 10:34:23 -0300

    Components and Pages - home page

--- 03-fundamentos-next/src/pages/index.tsx
+++ 03-fundamentos-next/src/pages/index.tsx
@@ -1,12 +1,25 @@
 import Head from 'next/head';
+import styles from './index.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello Word</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>News about <br/> the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
