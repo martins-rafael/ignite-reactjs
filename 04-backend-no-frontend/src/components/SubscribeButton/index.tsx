33265ae0b406c0bd5a5bc925d124f16770d4e55e
Rafael Martins <rafaeldcmartins@gmail.com>
2021-03-27 10:44:26 -0300

    Stripe Payment - stripe checkout session

--- 04-backend-no-frontend/src/components/SubscribeButton/index.tsx
+++ 04-backend-no-frontend/src/components/SubscribeButton/index.tsx
@@ -1,11 +1,27 @@
+import { signIn, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => (
-  <button className={styles.subscribeButton} type="button">
-    Subscribe now
-  </button>
-);
+export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+  }
+
+  return (
+    <button
+      className={styles.subscribeButton}
+      type="button"
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
+}
