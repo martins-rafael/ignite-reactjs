ff9e269ab56b07bf38624986a37bb1527736bfa3
Rafael Martins <rafaeldcmartins@gmail.com>
2021-06-30 10:45:28 -0300

    User Control - User form

---
+++ 06-construindo-interfaces-do-futuro/src/pages/users/create.tsx
@@ -0,0 +1,55 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Divider,
+  VStack,
+  HStack,
+  SimpleGrid,
+  Button
+} from "@chakra-ui/react";
+
+import { Input } from "../../components/Form/Input";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function CreateUser() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex my="6" mx="auto" px="6" w="100%" maxWidth={1480}>
+        <Sidebar />
+
+        <Box flex="1" p="8" bg="gray.800" borderRadius={8}>
+          <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
+
+          <Divider my="6" borderColor="gray.700" />
+
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="name" label="Nome completo" />
+              <Input name="email" type="email" label="E-mail" />
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="password" type="password" label="Senha" />
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirmar senha"
+              />
+            </SimpleGrid>
+
+            <Flex mt="8" w="100%">
+              <HStack spacing="4">
+                <Button colorScheme="whiteAlpha">Cancelar</Button>
+                <Button colorScheme="pink">Salvar</Button>
+              </HStack>
+            </Flex>
+          </VStack>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
