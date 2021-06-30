87ed7b5ce134a076d382897849bfb077c8111e07
Rafael Martins <rafaeldcmartins@gmail.com>
2021-06-30 10:14:02 -0300

    User Control - Pagination component

--- 06-construindo-interfaces-do-futuro/src/pages/users/index.tsx
+++ 06-construindo-interfaces-do-futuro/src/pages/users/index.tsx
@@ -17,6 +17,7 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
   return (
@@ -87,6 +88,8 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
