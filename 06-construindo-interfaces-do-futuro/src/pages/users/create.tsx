cf8c23eb423a87b8aab8f79996b4533bb04db208
Rafael Martins <rafaeldcmartins@gmail.com>
2021-07-05 09:15:22 -0300

    Responsive - User form

--- 06-construindo-interfaces-do-futuro/src/pages/users/create.tsx
+++ 06-construindo-interfaces-do-futuro/src/pages/users/create.tsx
@@ -21,18 +21,18 @@ export default function CreateUser() {
       <Flex my="6" mx="auto" px="6" w="100%" maxWidth={1480}>
         <Sidebar />
 
-        <Box flex="1" p="8" bg="gray.800" borderRadius={8}>
+        <Box flex="1" p={["6", "8"]} bg="gray.800" borderRadius={8}>
           <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
 
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="name" label="Nome completo" />
               <Input name="email" type="email" label="E-mail" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="password" type="password" label="Senha" />
               <Input
                 name="password_confirmation"
