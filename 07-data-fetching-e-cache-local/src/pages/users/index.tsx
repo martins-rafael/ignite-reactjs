7e3d653a6ed9bdab0d37dedeb41c3d9974a166ec
Rafael Martins <rafaeldcmartins@gmail.com>
2021-10-05 11:00:14 -0300

    React Query Concepts - Users listing

--- 07-data-fetching-e-cache-local/src/pages/users/index.tsx
+++ 07-data-fetching-e-cache-local/src/pages/users/index.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { useEffect } from 'react'
 import { useQuery } from 'react-query';
 import {
   Box,
@@ -28,9 +27,17 @@ export default function UserList() {
   const { data, isLoading, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
-    return data;
-  });
+    const users = data.users.map(user => ({
+      ...user,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }));
 
+    return users;
+  });
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -90,34 +97,36 @@ export default function UserList() {
                   </Thead>
 
                   <Tbody>
-                    <Tr>
-                      <Td px={["4", "4", "6"]}>
-                        <Checkbox colorScheme="pink" />
-                      </Td>
-
-                      <Td>
-                        <Box>
-                          <Text fontWeight="bold">Rafael Martins</Text>
-                          <Text fontSize="sm" color="gray.300">rafaeldcmartins@gmail.com</Text>
-                        </Box>
-                      </Td>
-
-                      {isWideVersion && <Td>29 de Junho, 2021</Td>}
-
-                      {isWideVersion && (
-                        <Td>
-                          <Button
-                            as="a"
-                            size="sm"
-                            fontSize="sm"
-                            colorScheme="pink"
-                            leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                          >
-                            Editar
-                          </Button>
+                    {data.map(user => (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
                         </Td>
-                      )}
-                    </Tr>
+
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
+
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+
+                        {isWideVersion && (
+                          <Td>
+                            <Button
+                              as="a"
+                              size="sm"
+                              fontSize="sm"
+                              colorScheme="pink"
+                              leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                            >
+                              Editar
+                            </Button>
+                          </Td>
+                        )}
+                      </Tr>
+                    ))}
                   </Tbody>
                 </Table >
 
