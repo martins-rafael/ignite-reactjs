7a56113eda706bdee78a41647bb968937a9bafde
Rafael Martins <rafaeldcmartins@gmail.com>
2021-10-06 14:46:41 -0300

    Building Application - Flagging data refetch

--- 07-data-fetching-e-cache-local/src/pages/users/index.tsx
+++ 07-data-fetching-e-cache-local/src/pages/users/index.tsx
@@ -24,7 +24,7 @@ import { Sidebar } from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error } = useQuery(
     'users',
     async () => {
       const response = await fetch('http://localhost:3000/api/users');
@@ -61,6 +61,7 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
             </Heading>
 
             <Link href="/users/create" passHref>
