8ec20fe934e5e5bc10f0e804ef91a2d858935ae3
Rafael Martins <rafaeldcmartins@gmail.com>
2021-10-27 17:01:37 -0300

    Building Application - Data Prefetch

--- 07-data-fetching-e-cache-local/src/pages/_app.tsx
+++ 07-data-fetching-e-cache-local/src/pages/_app.tsx
@@ -1,17 +1,16 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { theme } from '../styles/theme';
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { queryClient } from '../services/query-client';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- 07-data-fetching-e-cache-local/src/pages/users/index.tsx
+++ 07-data-fetching-e-cache-local/src/pages/users/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Link from "next/link";
+import NextLink from "next/link";
 import {
   Box,
   Button,
@@ -15,10 +15,13 @@ import {
   Checkbox,
   Text,
   useBreakpointValue,
-  Spinner
+  Spinner,
+  Link
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { useUsers } from '../../services/hooks/useUsers';
+import { queryClient } from '../../services/query-client';
+import { api } from '../../services/api';
 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
@@ -32,6 +35,15 @@ export default function UserList() {
     base: false,
     lg: true
   });
+
+  async function handlePrefetchUser(userId: number) {
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`users/${userId}`);
+      return response.data;
+    }, {
+      staleTime: 1000 * 60 * 10 // 10 minutes
+    });
+  }
 
   return (
     <Box>
@@ -47,7 +59,7 @@ export default function UserList() {
               {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
             </Heading>
 
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -57,7 +69,7 @@ export default function UserList() {
               >
                 Criar Novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
           {isLoading ? (
@@ -95,7 +107,9 @@ export default function UserList() {
 
                         <Td>
                           <Box>
-                            <Text fontWeight="bold">{user.name}</Text>
+                            <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(Number(user.id))}>
+                              <Text fontWeight="bold">{user.name}</Text>
+                            </Link>
                             <Text fontSize="sm" color="gray.300">{user.email}</Text>
                           </Box>
                         </Td>
