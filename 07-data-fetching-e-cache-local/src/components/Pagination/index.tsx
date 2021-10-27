a071f851c1034b3ad02291ecbada63a27d520eef
Rafael Martins <rafaeldcmartins@gmail.com>
2021-10-27 16:30:44 -0300

    Building Application - Switching pages

--- 07-data-fetching-e-cache-local/src/components/Pagination/index.tsx
+++ 07-data-fetching-e-cache-local/src/components/Pagination/index.tsx
@@ -47,7 +47,7 @@ export function Pagination({
       <Stack direction="row" spacing="2">
         {currentPage > (1 + siblingsCount) && (
           <>
-            <PaginationItem pageNumber={1} />
+            <PaginationItem onPageChange={onPageChange} pageNumber={1} />
             {currentPage > (2 + siblingsCount) && (
               <Text color="gray.300" width="8" textAlign="center">...</Text>
             )}
@@ -55,13 +55,13 @@ export function Pagination({
         )}
 
         {previousPages.length > 0 && previousPages.map(page => {
-          return <PaginationItem key={page} pageNumber={page} />
+          return <PaginationItem onPageChange={onPageChange} key={page} pageNumber={page} />
         })}
 
-        <PaginationItem pageNumber={currentPage} isCurrent />
+        <PaginationItem onPageChange={onPageChange} pageNumber={currentPage} isCurrent />
 
         {nextPages.length > 0 && nextPages.map(page => {
-          return <PaginationItem key={page} pageNumber={page} />
+          return <PaginationItem onPageChange={onPageChange} key={page} pageNumber={page} />
         })}
 
         {(currentPage + siblingsCount) < lastPage && (
@@ -69,7 +69,7 @@ export function Pagination({
             {(currentPage + 1 + siblingsCount) < lastPage && (
               <Text color="gray.300" width="8" textAlign="center">...</Text>
             )}
-            <PaginationItem pageNumber={lastPage} />
+            <PaginationItem onPageChange={onPageChange} pageNumber={lastPage} />
           </>
         )}
       </Stack>
--- 07-data-fetching-e-cache-local/src/pages/users/index.tsx
+++ 07-data-fetching-e-cache-local/src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Link from "next/link";
 import {
   Box,
@@ -24,7 +25,8 @@ import { Sidebar } from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers();
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -85,7 +87,7 @@ export default function UserList() {
                   </Thead>
 
                   <Tbody>
-                    {data.map(user => (
+                    {data.users.map(user => (
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]}>
                           <Checkbox colorScheme="pink" />
@@ -119,9 +121,9 @@ export default function UserList() {
                 </Table >
 
                 <Pagination
-                  totalCountOfRegisters={200}
-                  currentPage={5}
-                  onPageChange={() => {}}
+                  totalCountOfRegisters={data.totalCount}
+                  currentPage={page}
+                  onPageChange={setPage}
                 />
               </>
             )
