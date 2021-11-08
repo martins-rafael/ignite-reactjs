1dcb120ff9e4391d50be40a08688c4cb969a5785
Rafael Martins <rafaeldcmartins@gmail.com>
2021-11-08 16:58:02 -0300

    Auth with JWT - Retrieving auth state

--- 08-autenticacao-e-autorizacao/next-auth/contexts/AuthContext.tsx
+++ 08-autenticacao-e-autorizacao/next-auth/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import Router from "next/router";
-import { setCookie } from 'nookies';
+import { setCookie, parseCookies } from 'nookies';
 
 import { api } from "../services/api";
 
@@ -31,6 +31,21 @@ export default function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies();
+    if (token) {
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({
+          email,
+          permissions,
+          roles
+        });
+      });
+    }
+  }, []);
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post('sessions', {
@@ -53,6 +68,8 @@ export default function AuthProvider({ children }: AuthProviderProps) {
         permissions,
         roles
       });
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
       Router.push('/dashboard');
     } catch (err) {
--- 08-autenticacao-e-autorizacao/next-auth/pages/dashboard.tsx
+++ 08-autenticacao-e-autorizacao/next-auth/pages/dashboard.tsx
@@ -1,8 +1,16 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+
+import { api } from "../services/api";
 
 const Dashboard = () => {
   const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    api.get('/me').then(response => console.log(response)
+    );
+  }, []);
+
   return <h1>Dashboard {user?.email}</h1>;
 }
 export default Dashboard;
