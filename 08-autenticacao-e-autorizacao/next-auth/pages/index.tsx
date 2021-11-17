793dbb3d047baa40c2b904a40629fff6223a0313
Rafael Martins <rafaeldcmartins@gmail.com>
2021-11-17 15:14:47 -0300

    Auth with SSR - Retrieving token on SSR

--- 08-autenticacao-e-autorizacao/next-auth/pages/index.tsx
+++ 08-autenticacao-e-autorizacao/next-auth/pages/index.tsx
@@ -1,3 +1,5 @@
+import { GetServerSideProps } from 'next';
+import { parseCookies } from 'nookies';
 import { FormEvent, useContext, useState } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import styles from '../styles/Home.module.css'
@@ -6,7 +8,7 @@ const Home = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const {signIn} = useContext(AuthContext);
+  const { signIn } = useContext(AuthContext);
 
   async function handleSubimit(event: FormEvent) {
     event.preventDefault();
@@ -37,3 +39,20 @@ const Home = () => {
 }
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if (cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false
+      }
+    };
+  }
+
+  return {
+    props: {}
+  };
+}
