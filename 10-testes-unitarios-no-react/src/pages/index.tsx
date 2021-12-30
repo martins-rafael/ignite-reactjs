ca552bef756013a874a94be64588751750c7bd62
Rafael Martins <rafaeldcmartins@gmail.com>
2021-12-30 08:30:58 -0300

    Testing Pages - Testing Home page

--- 10-testes-unitarios-no-react/src/pages/index.tsx
+++ 10-testes-unitarios-no-react/src/pages/index.tsx
@@ -9,7 +9,7 @@ import styles from './index.module.scss';
 interface HomeProps {
   product: {
     priceId: string;
-    amount: number;
+    amount: string;
   }
 }
 
@@ -29,7 +29,7 @@ const Home = ({ product }: HomeProps) => {
             <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
